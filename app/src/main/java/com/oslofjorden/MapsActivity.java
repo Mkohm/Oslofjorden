eafee93289a000dc51e1a2ca6d8774ecb4a8fc47
Mkohm <marius.kohmann@gmail.com>
2016-02-09 19:58:16 +0100

    Removed unneccessary imports.

--- app/src/main/java/com/oslofjorden/MapsActivity.java
+++ app/src/main/java/com/oslofjorden/MapsActivity.java
@@ -1,7 +1,6 @@
 package com.oslofjorden;
 
 import android.Manifest;
-import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
