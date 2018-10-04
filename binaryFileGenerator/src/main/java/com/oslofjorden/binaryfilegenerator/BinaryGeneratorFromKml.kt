195284190ddf5c736fc4ed6a6c3dbca13464d835
Marius Kohmann <marius.kohmann@gmail.com>
2018-10-04 14:56:07 +0200

    added some work on the binarygenerator

--- binaryFileGenerator/src/main/java/com/oslofjorden/binaryfilegenerator/BinaryGeneratorFromKml.kt
+++ binaryFileGenerator/src/main/java/com/oslofjorden/binaryfilegenerator/BinaryGeneratorFromKml.kt
@@ -2,20 +2,27 @@ package com.oslofjorden.binaryfilegenerator
 
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Element
-import java.io.BufferedReader
-import java.io.File
-import java.io.InputStreamReader
-import java.io.Serializable
+import java.io.*
 
+
+// To run this first build the project separately and then run the file
+fun main(args: Array<String>) {
+    val generator = BinaryGeneratorFromKml()
+    val data = generator.parseKMLAndOutputLists()
+    generator.writeBinaryFile(data)
+}
 
 class BinaryGeneratorFromKml {
+
 
     fun parseKMLAndOutputLists(): Array<java.util.ArrayList<out Serializable?>> {
 
         val names = ArrayList<String>()
         val descriptions = ArrayList<String>()
+        val colors = ArrayList<String>()
         val links = ArrayList<String?>()
-        val coordinatesList = ArrayList<ArrayList<DoubleArray>>()
+
+        val coordinatesList = ArrayList<ArrayList<Pair<Double, Double>>>()
 
 
         val path = System.getProperty("user.dir")
@@ -28,15 +35,40 @@ class BinaryGeneratorFromKml {
 
         for (placemark in element) {
             val title = placemark.select("name").text()
-            val description = placemark.select("description").text()
+            names.add(title)
+
+            var description = placemark.select("description").text()
+
+            val link = getLink(description)
+            links.add(link)
+
+            if (description.contains("<")) {
+                description = description.substring(0, description.indexOf("<"))
+            }
+
+            descriptions.add(description)
+
+
             val color = getColor(placemark)
+            colors.add(color ?: "ffffffff")
+
             val coordinates = getCoordinates(placemark)
-            println(coordinates)
-            println()
+            coordinatesList.add(coordinates)
         }
 
+        println("All these should be the same size or something will fail")
+        println(names.size)
+        println(descriptions.size)
+        println(links.size)
+        println(colors.size)
+        println(coordinatesList.size)
 
-        return arrayOf(names, descriptions, links, coordinatesList)
+        return arrayOf(names, descriptions, links, colors, coordinatesList)
+    }
+
+    private fun getLink(description: String): String {
+        val regex = """<a href="(.*)">""".toRegex()
+        return regex.find(description)?.groupValues?.get(1) ?: "null"
     }
 
     fun getColor(placemark: Element): String? {
@@ -46,10 +78,10 @@ class BinaryGeneratorFromKml {
 
     }
 
-    fun getCoordinates(placemark: Element): List<List<Double>> {
+    fun getCoordinates(placemark: Element): ArrayList<Pair<Double, Double>> {
         val coordinateString = placemark.select("LineString").select("coordinates").text()
 
-        val result = ArrayList<ArrayList<Double>>()
+        val result = ArrayList<Pair<Double, Double>>()
 
 
         val coordinatePairs = coordinateString.split(" ")
@@ -61,20 +93,33 @@ class BinaryGeneratorFromKml {
             val latitude = coordinates[0].toDouble()
             val longitude = coordinates[1].toDouble()
 
-            val finalList = ArrayList<Double>()
-            finalList.add(latitude)
-            finalList.add(longitude)
+            val pair = Pair(latitude, longitude);
 
-            result.add(finalList)
+
+            result.add(pair)
         }
 
 
         return result
     }
 
-}
+    //Writes objects to the file, two objects at each iteration, so when reading it can be checked if the thread is cancelled
+    fun writeBinaryFile(data: Array<java.util.ArrayList<out Serializable>>) {
 
-fun main(args: Array<String>) {
-    val generator = BinaryGeneratorFromKml()
-    generator.parseKMLAndOutputLists()
+        val fileout = FileOutputStream("polylines_binary_file.bin")
+        val out = ObjectOutputStream(fileout)
+
+        for (i in 0 until data[0].size) {
+            out.writeObject(data[0].get(i))
+            out.writeObject(data[1].get(i))
+            out.writeObject(data[2].get(i))
+            out.writeObject(data[3].get(i))
+            out.writeObject(data[4].get(i))
+        }
+
+
+        out.close()
+    }
+
+
 }
