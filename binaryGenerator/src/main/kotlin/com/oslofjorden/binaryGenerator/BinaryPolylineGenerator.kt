f1323d3281e8e8b648503be4d1e6a76751f7e0d5
Marius Kohmann <marius.kohmann@gmail.com>
2019-02-09 13:46:17 +0100

    Load all polylines (#35)
    * Update README.md
    
    * remove files
    
    * loading all polylines

--- binaryGenerator/src/main/kotlin/com/oslofjorden/binaryGenerator/BinaryPolylineGenerator.kt
+++ binaryGenerator/src/main/kotlin/com/oslofjorden/binaryGenerator/BinaryPolylineGenerator.kt
@@ -1,6 +1,7 @@
-package com.oslofjorden.binaryfilegenerator
+package com.oslofjorden.binaryGenerator
 
 import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
 import java.io.BufferedReader
 import java.io.File
@@ -9,15 +10,7 @@ import java.io.InputStreamReader
 import java.io.ObjectOutputStream
 import java.io.Serializable
 
-// Be sure to not build before running this - that will make an error.
-fun main() {
-    val generator = BinaryGeneratorFromKml()
-    val data = generator.parseKMLAndOutputLists()
-    generator.writeBinaryFile(data)
-}
-
-class BinaryGeneratorFromKml {
-
+class BinaryPolylineGenerator {
     fun parseKMLAndOutputLists(): Array<java.util.ArrayList<out Serializable?>> {
 
         val names = ArrayList<String>()
@@ -28,15 +21,18 @@ class BinaryGeneratorFromKml {
         val coordinatesList = ArrayList<ArrayList<Pair<Double, Double>>>()
 
         val path = System.getProperty("user.dir")
-        val reader =
-            BufferedReader(InputStreamReader(File(path + "/binaryFileGenerator/src/main/res/mapData" + "/turer_oslofjorden.kml").inputStream()))
-        val text = reader.readText()
+        val reader = BufferedReader(InputStreamReader(File("$path/binaryGenerator/src/main/res/mapData/doc.kml").inputStream()))
 
+        val text = reader.readText()
         val doc = Jsoup.parse(text)
 
-        val element = doc.select("Placemark")
+        val polylineElements = doc.getElementsByTag("Placemark")
 
-        for (placemark in element) {
+        for (placemark in polylineElements) {
+
+            // First find the styleUrlId of the placemark element
+            val color = getColor(placemark, doc)
+
             val title = placemark.select("name").text()
             names.add(title)
 
@@ -51,8 +47,7 @@ class BinaryGeneratorFromKml {
 
             descriptions.add(description)
 
-            val color = getColor(placemark)
-            colors.add(color ?: "ffffffff")
+            colors.add(color)
 
             val coordinates = getCoordinates(placemark)
             coordinatesList.add(coordinates)
@@ -68,15 +63,56 @@ class BinaryGeneratorFromKml {
         return arrayOf(names, descriptions, links, colors, coordinatesList)
     }
 
+    private fun getColor(placemark: Element, doc: Document): String {
+        val styleUrlId = placemark.getElementsByTag("styleUrl")[0].text().substring(1)
+
+        // The corresponding stylemap with this id is
+        val styleMap = doc.getElementsByAttributeValue("id", styleUrlId)
+
+        // This style contains two StyleUrl's one for when the polyline is highlighted and one for when the polyline is not clicked. We want to select the one where it is not clicked.
+        val styleNotClicked =
+            styleMap.get(0).getElementsByTag("Pair")[0].getElementsByTag("styleUrl").text().substring(1)
+
+        val style = doc.getElementsByAttributeValue("id", styleNotClicked)
+
+        val styleText = style[0].toString()
+
+        val regex = """<color>(.{8})</color>""".toRegex()
+
+        val color = regex.find(styleText)?.groupValues?.get(1)
+
+        if (color == null) {
+            return "blue"
+        } else {
+
+
+            val rgbColor = convertFromAABBGGRRToRRGGBB(color)
+
+            return "#$rgbColor"
+        }
+    }
+
+    private fun convertFromAABBGGRRToRRGGBB(color: String): String {
+        val blue = color.substring(2, 4)
+        val green = color.substring(4, 6)
+        val red = color.substring(6, 8)
+
+        return "$red$green$blue"
+    }
+
     private fun getLink(description: String): String {
         val regex = """<a href="(.*)">""".toRegex()
         return regex.find(description)?.groupValues?.get(1) ?: "null"
     }
 
-    fun getColor(placemark: Element): String? {
-        val style = placemark.select("Style").toString()
+    fun getColor(placemark: Element): String {
+        val style = placemark.toString()
+
         val regex = """<color>(.{8})</color>""".toRegex()
-        return regex.find(style)?.groupValues?.get(1)
+        val colorFromString = regex.find(style)?.groupValues?.get(1)
+
+
+        return if (colorFromString == null) "blue" else "#$colorFromString"
     }
 
     fun getCoordinates(placemark: Element): ArrayList<Pair<Double, Double>> {
