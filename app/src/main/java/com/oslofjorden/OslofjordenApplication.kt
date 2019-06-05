8bc8ba114d2b110ad60a1923e79018dbdd4a11a6
Marius Kohmann <marius.kohmann@gmail.com>
2019-06-05 14:26:06 +0200

    fix lint

--- app/src/main/java/com/oslofjorden/OslofjordenApplication.kt
+++ app/src/main/java/com/oslofjorden/OslofjordenApplication.kt
@@ -4,15 +4,13 @@ import android.app.Application
 import com.google.firebase.FirebaseApp
 import com.google.firebase.messaging.FirebaseMessaging
 
+@Suppress("Unused")
 class OslofjordenApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
 
-
         FirebaseApp.initializeApp(applicationContext)
         FirebaseMessaging.getInstance().subscribeToTopic("weather")
     }
-
-
 }
